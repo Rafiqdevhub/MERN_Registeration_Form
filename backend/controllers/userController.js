24201877df566e033706127a223ae19f004a685e
Muhammad Rafiq <rafkhan9323@gmail.com>
2024-08-08 07:44:50 +0500

    Use const for user variable in registerUser function

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -6,7 +6,7 @@ const registerUser = async (req, res) => {
   const { username, email, password } = req.body;
 
   try {
-    let user = await User.findOne({ email });
+    const user = await User.findOne({ email });
     if (user) {
       return res.status(400).json({ message: "User already exists" });
     }
